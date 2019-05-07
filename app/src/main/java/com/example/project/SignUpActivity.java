bf03a64ecc55baf31ae5100adf20f4b99048fb7f
efung001 <efung001@ucr.edu>
2019-05-07 15:10:22 -0700

    More Fields for User Information
    will create array for each of the days

--- app/src/main/java/com/example/project/SignUpActivity.java
+++ app/src/main/java/com/example/project/SignUpActivity.java
@@ -1,9 +1,9 @@
 package com.example.project;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -11,10 +11,17 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import static com.google.firebase.auth.FirebaseAuth.getInstance;
 
@@ -25,9 +32,11 @@ public class SignUpActivity extends AppCompatActivity {
     private Button SignUpDone;
     private FirebaseAuth Authentication;
     private static final String TAG = "SignUpActivity";
-
+    private String uid = "";
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         Authentication = getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
@@ -45,24 +54,52 @@ public class SignUpActivity extends AppCompatActivity {
                         .addOnCompleteListener(SignUpActivity.this, new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
+                                Intent myIntent;
                                 if (task.isSuccessful()) {
                                     // Sign in success, update UI with the signed-in user's information
                                     Log.d(TAG, "createUserWithEmail:success");
                                     FirebaseUser user = Authentication.getCurrentUser();
-                                    Intent myIntent = new Intent(SignUpActivity.this, HomeScreen.class);
-                                    startActivity(myIntent);
+                                    uid = user.getUid();
+                                    helper(uid);
+                                    myIntent = new Intent(SignUpActivity.this, HomeScreen.class);
                                 } else {
                                     // If sign in fails, display a message to the user.
                                     Log.w(TAG, "createUserWithEmail:failure", task.getException());
                                     Toast.makeText(SignUpActivity.this, "Authentication failed.",
                                             Toast.LENGTH_SHORT).show();
                                     //updateUI(null);
-                                    Intent myIntent = new Intent(SignUpActivity.this, SignUpActivity.class);
-                                    startActivity(myIntent);
+                                    myIntent = new Intent(SignUpActivity.this, SignUpActivity.class);
                                 }
+                                startActivity(myIntent);
                             }
                         });
             }
         });
+    }
+    private void helper(String UID){
+        Map<String,Object> Data= new HashMap<>();
+        Data.put("First Name", "tester1");
+        Data.put("Last Name", "");
+        Data.put("Phone Number", "");
+        Data.put("Monday", Arrays.asList("N/A"));
+        Data.put("Tuesday", Arrays.asList("15:00-Chung"));
+        Data.put("Wednesday", Arrays.asList("N/A"));
+        Data.put("Thursday", Arrays.asList("N/A"));
+        Data.put("Friday", Arrays.asList("N/A"));
+
+        db.collection("User_Information").document(UID)
+                .set(Data)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "DocumentSnapshot successfully written!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error writing document", e);
+                    }
+                });
     }
 }
