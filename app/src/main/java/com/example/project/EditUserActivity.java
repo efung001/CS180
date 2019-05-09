49ba8063f467c4e8b481b48d683e79902f2171c1
efung001 <efung001@ucr.edu>
2019-05-08 17:15:21 -0700

    Changed Hello Message

--- app/src/main/java/com/example/project/EditUserActivity.java
+++ app/src/main/java/com/example/project/EditUserActivity.java
@@ -2,18 +2,12 @@ package com.example.project;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 
@@ -21,14 +15,17 @@ public class EditUserActivity extends AppCompatActivity {
 
     private Button Personal_Update, ScheduleUpdate;
     private FirebaseAuth Authentication;
+    private FirebaseUser user;
     private static final String TAG = "EditUserActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         Authentication = FirebaseAuth.getInstance();
-        String uid = Authentication.getCurrentUser().getUid();
+        user = Authentication.getCurrentUser();
+        String uid = user.getUid();
 
+        /*
         DocumentReference docRef = db.collection("User_Information").document(uid);
         docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
@@ -46,9 +43,11 @@ public class EditUserActivity extends AppCompatActivity {
                     Log.d(TAG, "get failed with ", task.getException());
                 }
             }
-        });
+        });*/
 
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_user);
+
         Personal_Update = findViewById(R.id.button);
         Personal_Update.setOnClickListener(new View.OnClickListener() {
             @Override
